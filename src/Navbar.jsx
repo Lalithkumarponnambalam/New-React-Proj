84867cb1c54542a548ff7d08b8ea7fbe21d30d2f
Lalithkumarponnambalam <139358124+Lalithkumarponnambalam@users.noreply.github.com>
2025-01-20 10:26:09 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <Link className="navbar-brand" to="/">
           My React App
         </Link>
-        <a
+        <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -29,7 +29,7 @@ function Navbar() {
           >
             <path d="M24 21v2c0 0.547-0.453 1-1 1h-22c-0.547 0-1-0.453-1-1v-2c0-0.547 0.453-1 1-1h22c0.547 0 1 0.453 1 1zM24 13v2c0 0.547-0.453 1-1 1h-22c-0.547 0-1-0.453-1-1v-2c0-0.547 0.453-1 1-1h22c0.547 0 1 0.453 1 1zM24 5v2c0 0.547-0.453 1-1 1h-22c-0.547 0-1-0.453-1-1v-2c0-0.547 0.453-1 1-1h22c0.547 0 1 0.453 1 1z"></path>
           </svg>
-        </a>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
